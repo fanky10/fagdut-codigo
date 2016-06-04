08e9b717c0561714783cc9ba8986b6dc8d32e390
Facundo Scoccia <fanky10@gmail.com>
2016-06-04 08:59:22 -0300

    Ej Basico B - Inicial

---
+++ EjBasicoB/app/src/main/java/ar/org/fagdut/codigo/android/ejbasicob/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.org.fagdut.codigo.android.ejbasicob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
