74cd0a1a300fd9417ebb7668d104f1ff95dd82a2
Facundo Scoccia <fanky10@gmail.com>
2016-10-29 08:49:40 -0300

    Add Ejercicio_Base

---
+++ Ejercicio_Base/app/src/main/java/ar/org/fagdut/android/codigo/ejercicio_base/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.org.fagdut.android.codigo.ejercicio_base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
